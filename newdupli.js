721a331aa9d5a5b2bd851d81e77165b6151157e5
Tahir Gozuacik <136073736+tahirsercan@users.noreply.github.com>
2025-02-06 01:23:21 +0300

    Create newdupli.js

---
+++ newdupli.js
@@ -0,0 +1,152 @@
+// duplicated-code.js
+function calculateSum(a, b) {
+  return a + b;
+}
+
+function calculateDifference(a, b) {
+  return a - b;
+}
+
+function calculateProduct(a, b) {
+  return a * b;
+}
+
+function calculateQuotient(a, b) {
+  return a / b;
+}
+
+// Aynı kod bloklarının tekrarı
+function calculateSumAgain(a, b) {
+  return a + b;
+}
+
+function calculateDifferenceAgain(a, b) {
+  return a - b;
+}
+
+function calculateProductAgain(a, b) {
+  return a * b;
+}
+
+function calculateQuotientAgain(a, b) {
+  return a / b;
+}
+
+// Daha fazla tekrar
+function addNumbers(x, y) {
+  return x + y;
+}
+
+function subtractNumbers(x, y) {
+  return x - y;
+}
+
+function multiplyNumbers(x, y) {
+  return x * y;
+}
+
+function divideNumbers(x, y) {
+  return x / y;
+}
+
+// Aynı işlevlerin farklı isimlerle tekrarı
+function sum(a, b) {
+  return a + b;
+}
+
+function difference(a, b) {
+  return a - b;
+}
+
+function product(a, b) {
+  return a * b;
+}
+
+function quotient(a, b) {
+  return a / b;
+}
+
+// Daha fazla tekrar
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Aynı işlevlerin farklı parametrelerle tekrarı
+function computeSum(x, y) {
+  return x + y;
+}
+
+function computeDifference(x, y) {
+  return x - y;
+}
+
+function computeProduct(x, y) {
+  return x * y;
+}
+
+function computeQuotient(x, y) {
+  return x / y;
+}
+
+// Daha fazla tekrar
+function getSum(a, b) {
+  return a + b;
+}
+
+function getDifference(a, b) {
+  return a - b;
+}
+
+function getProduct(a, b) {
+  return a * b;
+}
+
+function getQuotient(a, b) {
+  return a / b;
+}
+
+// Aynı işlevlerin farklı isimlerle tekrarı
+function performAddition(a, b) {
+  return a + b;
+}
+
+function performSubtraction(a, b) {
+  return a - b;
+}
+
+function performMultiplication(a, b) {
+  return a * b;
+}
+
+function performDivision(a, b) {
+  return a / b;
+}
+
+// Daha fazla tekrar
+function addValues(a, b) {
+  return a + b;
+}
+
+function subtractValues(a, b) {
+  return a - b;
+}
+
+function multiplyValues(a, b) {
+  return a * b;
+}
+
+function divideValues(a, b) {
+  return a / b;
+}
