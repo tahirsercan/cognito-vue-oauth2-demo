d32d5d36c73b37b2c3ee5f35b430aa6dca05f3fd
Tahir Gozuacik <136073736+tahirsercan@users.noreply.github.com>
2025-02-06 01:28:15 +0300

    Create high-duplication.js

---
+++ high-duplication.js
@@ -0,0 +1,131 @@
+// high-duplication.js
+
+// Toplama işlevleri
+
+function sum1(a, b) {
+
+return a + b;
+
+}
+
+function sum2(x, y) {
+
+return x + y;
+
+}
+
+function addValues(num1, num2) {
+
+return num1 + num2;
+
+}
+
+function calculateTotal(valueA, valueB) {
+
+return valueA + valueB;
+
+}
+
+// Çıkarma işlevleri
+
+function subtract1(a, b) {
+
+return a - b;
+
+}
+
+function subtract2(x, y) {
+
+return x - y;
+
+}
+
+function removeValues(num1, num2) {
+
+return num1 - num2;
+
+}
+
+function calculateDifference(valueA, valueB) {
+
+return valueA - valueB;
+
+}
+
+// Çarpma işlevleri
+
+function multiply1(a, b) {
+
+return a * b;
+
+}
+
+function multiply2(x, y) {
+
+return x * y;
+
+}
+
+function productOfValues(num1, num2) {
+
+return num1 * num2;
+
+}
+
+function computeProduct(valueA, valueB) {
+
+return valueA * valueB;
+
+}
+
+// Bölme işlevleri
+
+function divide1(a, b) {
+
+return a / b;
+
+}
+
+function divide2(x, y) {
+
+return x / y;
+
+}
+
+function splitValues(num1, num2) {
+
+return num1 / num2;
+
+}
+
+function calculateQuotient(valueA, valueB) {
+
+return valueA / valueB;
+
+}
+
+// Aynı işlevlerin farklı parametre isimleriyle tekrarı
+
+function computeSum(a, b) {
+
+return a + b;
+
+}
+
+function performAddition(x, y) {
+
+return x + y;
+
+}
+
+function getSum(num1, num2) {
+
+return num1 + num2;
+
+}
+
+function totalOfValues(valueA, valueB) {
+
+return valueA + valueB;
+
+}
